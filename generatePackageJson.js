8a056542acbfae2cb82789bd9ba886c0a5703e01
Timothy Johnson <timothy.johnson@broadcom.com>
2023-01-24 09:28:39 -0500

    Replace plugin list with Theia extension pack
    Signed-off-by: Timothy Johnson <timothy.johnson@broadcom.com>

--- generatePackageJson.js
+++ generatePackageJson.js
@@ -13,22 +13,10 @@ async function getTheiaVersion() {
 
 async function getTheiaPlugins(theiaVersion) {
     const theiaGitRef = theiaVersion !== "next" ? `v${theiaVersion}` : "master";
-    let response = await fetch(`https://raw.githubusercontent.com/eclipse-theia/theia/${theiaGitRef}/dev-packages/application-package/src/api.ts`);
-    const vscodeVersion = (await response.text()).match(/DEFAULT_SUPPORTED_API_VERSION = '(\d+\.\d+\.\d+)'/)[1];
-    fs.appendFileSync(outputPath, `VSCODE_VERSION=${vscodeVersion}\n`);
-    response = await fetch("https://open-vsx.org/api/vscode/");
-    const data = await response.json();
-    const theiaPlugins = {};
-    for (const [k, v] of Object.entries(data.extensions)) {
-        const extensionName = `vscode-builtin-${k}`;
-        response = await fetch(`${v}/${vscodeVersion}`);
-        if (response.ok) {
-            theiaPlugins[extensionName] = `${v}/${vscodeVersion}/file/vscode.${k}-${vscodeVersion}.vsix`;
-        }
-        console.log((response.ok ? "✔️ " : "❌ ") + extensionName);
-        await new Promise(resolve => setTimeout(resolve, 100));
-    }
-    return theiaPlugins;
+    const response = await fetch(`https://raw.githubusercontent.com/eclipse-theia/theia/${theiaGitRef}/package.json`);
+    const theiaPlugins = (await response.json()).theiaPlugins;
+    const builtinExtensionPack = "eclipse-theia.builtin-extension-pack";
+    return { [builtinExtensionPack]: theiaPlugins[builtinExtensionPack] };
 }
 
 (async () => {
